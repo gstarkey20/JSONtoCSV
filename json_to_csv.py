54a0de438e0f61702162317cbeb76211acae6634
gstarkey20 <gstarkey1@catamount.wcu.edu>
2021-01-25 20:32:53 -0500

    remove file

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,6 +1,12 @@
 # This program is intended to convert from JSON format to CSV format.
 
+import sys
+import os
+
 
 def json_to_csv():
-	# TODO
+	entries = os.listdir('/Users/garrettstarkey/Desktop/PersonalProjects/JSONtoCSV')
+	for entry in entries:
+		print(entry)	
 
+json_to_csv()
