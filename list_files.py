50d735d11e66cbc66763f13a9a31d27807df87f3
gstarkey20 <gstarkey1@catamount.wcu.edu>
2021-01-25 20:42:10 -0500

    updated documentation

--- list_files.py
+++ list_files.py
@@ -2,14 +2,13 @@
 # files with an .json extension. The file names will then be printed to 
 # the console
 
-
 import sys
 import os
 
-
+# locate directory, print the name of all files within
 def list_files():
 	entries = os.listdir('/Users/garrettstarkey/Desktop/PersonalProjects/JSONtoCSV')
-	for entry in entries:
+	for entry in entries:  # loop through entire directrory
 		print(entry)	
 
 list_files()
